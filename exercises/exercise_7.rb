b40392a0f6c4491ae59d5c3ffaf0c52cd8f97b20
Vidushan Kohiladas <vidushan@Vidushans-MacBook-Air.local>
2017-08-01 19:23:12 -0700

    completed exercise7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,17 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
+# 2. Ask the user for a store name (store it in a variable)
+# 3. Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+# 4. Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "ENTER a name"
+
+params = {name: gets.chomp}
+
+store = Store.create(params)
+
+puts store.errors.full_messages
